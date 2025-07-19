f2468983378ae83b4d5fbb36c87bb16a20919c38
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-07-19 19:25:19 -0300

    add SearchBar component and integrate it into Header; include search icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import {styled} from 'styled-components';
+import SearchBar from '../SearchBar';
 
 const StyledHeader = styled.header`
   padding: 60px 0;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 
   img {
     max-width: 212px;
@@ -14,6 +16,7 @@ function Header() {
   return (
     <StyledHeader>
       <img src="/images/logo.png" alt="Space App Logo" />
+      <SearchBar />
     </StyledHeader>
   );
 }
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const SearchContainer = styled.div`
+  display: flex;
+  position: relative;
+  display: inline-block;
+`;
+
+const SearchBarStyled = styled.input`
+  display: flex;
+  height: 56px;
+  padding: 12px 16px;
+  border-radius: 10px;
+  border: 2px solid;
+  border-color: #c98cf1;
+  background: transparent;
+  box-sizing: border-box;
+  width: 566px;
+  color: #d9d9d9;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 20px;
+`;
+
+const IconSearch = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 38px;
+  height: 38px;
+`;
+
+function SearchBar() {
+  return (
+    <SearchContainer>
+      <SearchBarStyled type="text" placeholder="Pesquisar..." />
+      <IconSearch src="/icons/search.png" alt="Icone de uma lupa" />
+    </SearchContainer>
+  );
+}
+
+export default SearchBar;
